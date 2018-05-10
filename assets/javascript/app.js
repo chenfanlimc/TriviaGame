3337ddab48e9bac7b9185587b0380f6285da43de
cli <cli@mastercontrol.com>
2018-05-09 18:23:15 -0600

    added some basic html css and started java

---
+++ assets/javascript/app.js
@@ -0,0 +1,34 @@
+//wait for everything to load before even doing stuff
+$(document).ready(function() {
+    
+    var questionOne = {
+        question: "What does the ancient symbol Ouroboros classically look like?",
+        answersArray: ["A spear with a criss-crossing hilt", "A flying dragon that lifts the world", 
+        "A serpent that eats its own tail", "A beautiful princess surrounded by fire"]
+    }
+    var questionTwo = {
+        question: "From what ancient Chinese epic did the Monkey King originate from?",
+        answersArray: ["Journey to the West", "Romance of the Three Kingdoms", "Dream of the Red Chamber", "Water Margin"]
+    }
+    var questionThree = {
+        question: "In the Japanese mythology of Susanoo and Orochi, what is the name of the sword that slays the legednary serpent?",
+        answersArray: ["Demon Cutter of Seven Curses", "Sword of the Gathering Clouds of Heaven", 
+        "Swallow Blade of Divine Wind", "Death-Culling Spirit Blade"]
+    }
+    var questionFour = {
+        question: "What is the primary purpose of Yggdrasil, the mythical tree of Norse Mythology?",
+        answersArray: ["It is the core of the cosmos and connects all the worlds", "It is a flying tree that houses various Norse gods", 
+        "It is a legendary source of ingredients for the mightiest weapons", "It is a mythical staff wielded by Odin during Ragnorak"]
+    }
+    var questionFive = {
+        question: "In Greek Mythology, how many Titans were originally borne from Gaia?",
+        answersArray: ["6", "10", "12", "15"]
+    }
+
+
+    
+
+
+
+
+});
