4289caf000797d4f0e779d329896fab8c4bf0a30
cli <cli@mastercontrol.com>
2018-05-12 12:47:08 -0600

    made some progress on app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,60 +1,67 @@
 //wait for everything to load before even doing stuff
-$(document).ready(function() {
-    
-    var questionOne = {
-        question: "What does the ancient symbol Ouroboros classically look like?",
-        answersArray: ["A spear with a criss-crossing hilt", "A flying dragon that lifts the world", 
-        "A serpent that eats its own tail", "A beautiful princess surrounded by fire"],
-        answer: 2
-    }
-    var questionTwo = {
-        question: "From what ancient Chinese epic did the Monkey King originate from?",
-        answersArray: ["Journey to the West", "Romance of the Three Kingdoms", "Dream of the Red Chamber", "Water Margin"],
-        answer: 0
-    }
-    var questionThree = {
-        question: "In the Japanese mythology of Susanoo and Orochi, what is the name of the sword that slays the legednary serpent?",
-        answersArray: ["Demon Cutter of Seven Curses", "Sword of the Gathering Clouds of Heaven", 
-        "Swallow Blade of Divine Wind", "Death-Culling Spirit Blade"],
-        answer: 1
-    }
-    var questionFour = {
-        question: "What is the primary purpose of Yggdrasil, the mythical tree of Norse Mythology?",
-        answersArray: ["It is the core of the cosmos and connects all the worlds", "It is a flying tree that houses various Norse gods", 
-        "It is a legendary source of ingredients for the mightiest weapons", "It is a mythical staff wielded by Odin during Ragnorak"],
-        answer: 0
-    }
-    var questionFive = {
-        question: "In Greek Mythology, how many Titans were originally borne from Gaia?",
-        answersArray: ["6", "10", "12", "15"],
-        answer: 2
-    }
-    var questionSix = {
-        question: "In The Epic of Gilgamesh, why did the gods send Enkidu to Gilgamesh?",
-        answersArray: ["To become his life-long friend", "To inspire him to do great things", 
-        "To aid him in the battle against the Bull of Heaven", "To humble him of his arrogance"],
-        answer: 3
-    }
 
-    function checkAnswer() {
-        console.log("checkAnswer ran");
+var questionOne = {
+    question: "What does the ancient symbol Ouroboros classically look like?",
+    answersArray: ["A spear with a criss-crossing hilt", "A flying dragon that lifts the world",
+        "A serpent that eats its own tail", "A beautiful princess surrounded by fire"],
+    answer: 2
+}
+var questionTwo = {
+    question: "From what ancient Chinese epic did the Monkey King originate from?",
+    answersArray: ["Journey to the West", "Romance of the Three Kingdoms", "Dream of the Red Chamber", "Water Margin"],
+    answer: 0
+}
+var questionThree = {
+    question: "In the Japanese mythology of Susanoo and Orochi, what is the name of the sword that slays the legednary serpent?",
+    answersArray: ["Demon Cutter of Seven Curses", "Sword of the Gathering Clouds of Heaven",
+        "Swallow Blade of Divine Wind", "Death-Culling Spirit Blade"],
+    answer: 1
+}
+var questionFour = {
+    question: "What is the primary purpose of Yggdrasil, the mythical tree of Norse Mythology?",
+    answersArray: ["It is the core of the cosmos and connects all the worlds", "It is a flying tree that houses various Norse gods",
+        "It is a legendary source of ingredients for the mightiest weapons", "It is a mythical staff wielded by Odin during Ragnorak"],
+    answer: 0
+}
+var questionFive = {
+    question: "In Greek Mythology, how many Titans were originally borne from Gaia?",
+    answersArray: ["6", "10", "12", "15"],
+    answer: 2
+}
+var questionSix = {
+    question: "In The Epic of Gilgamesh, why did the gods send Enkidu to Gilgamesh?",
+    answersArray: ["To become his life-long friend", "To inspire him to do great things",
+        "To aid him in the battle against the Bull of Heaven", "To humble him of his arrogance"],
+    answer: 3
+}
+
+var gameNavigation = {
+    actionClick: function (question) {
+        $(".answerButtons").on("click", function () {
+            if ($(this).val() === question.answersArray[question.answer]) {
+                console.log("Correct answer");
+            } else {
+                console.log("Wrong answer");
+            }
+
+            
+        })
     }
-    
+}
+
+$(document).ready(function() {
+
     $(".start").on("click", function() {
         $(".contentSection").html("<div class='questionContainer'></div>");
         $(".questionContainer").html(questionOne.question)
-        for(var i = 0; i < questionOne.answersArray.length; i++){
-            var button = $('<input type="button" onclick="' + checkAnswer + ' value="' + questionOne.answersArray[i] + '"/>')
+        for (var i = 0; i < questionOne.answersArray.length; i++) {
+            var button = $('<input type="button" class= "answerButtons" value="' + questionOne.answersArray[i] + '"/>')
             $(".contentSection").append(button);
         }
-        
+        gameNavigation.actionClick(questionOne);
+
     })
-
-
-
     
 
 
-
-
-});
+})
